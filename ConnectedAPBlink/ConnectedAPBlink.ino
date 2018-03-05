18466fc12ecdd79bdf4aeeb512145585a44327b5
Julio Melo <julio.engcomp@gmail.com>
2018-03-05 15:41:26 -0300

    Adding connected AP and Blink examples

---
+++ ConnectedAPBlink/ConnectedAPBlink.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "nome da rede";
+const char *password = "senha";
+
+ESP8266WebServer server(80);
+
+/* Teste de mensagem, conecte no node e entre na página com o ip dele
+ * via browser.
+ */
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configurando AP");
+  /* se você remover o parâmetro de password para criar uma rede aberta*/
+  WiFi.softAP(ssid, password);
+
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("Meu endereço IP: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Iniciado Servidor http teste");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ ConnectedBlink/ConnectecBlink.ino
@@ -0,0 +1,41 @@
+/*
+ ESP8266 Blink by Simon Peter
+ Blink the blue LED on the ESP-01 module
+ This example code is in the public domain
+ 
+ The blue LED on the ESP-01 module is connected to GPIO1 
+ (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+ 
+ Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+
+#include <ESP8266WiFi.h>
+
+char *ssid = "nome da rede";
+char *password = "senha";
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+  /*connect to WiFi*/
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Conectado à Wifi");
+  Serial.print("Meu Ip é: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
